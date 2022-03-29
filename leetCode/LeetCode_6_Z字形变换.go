b9a7b87456ecebfbc308f3cb33b0b38d0b43d7c8
Lil <1378951829@qq.com>
2022-03-29 17:27:30 +0800

    LCode 6

---
+++ leetCode/LeetCode_6_Z字形变换.go
@@ -0,0 +1,29 @@
+package main
+
+func convert(s string, n int) string {
+	if n == 1 {
+		return s
+	}
+
+	res := ""
+	for i := 0; i < n; i++ {
+		if i == 0 || i == n-1 {
+			for j := i; j < len(s); j += 2*n - 2 {
+				res += string(s[j])
+			}
+		} else {
+			for j, k := i, 2*n-2-i; j < len(s) || k < len(s); j, k = j+2*n-2, k+2*n-2 {
+				if j < len(s) { //这里要注意判定
+					res += string(s[j])
+				}
+
+				if k < len(s) {
+					res += string(s[k])
+				}
+			}
+		}
+	}
+
+	return res
+
+}
